38f3145fa2c5ce07cdc87bcabadaff7b9219d4f3
Mikhail Nepochatov <m380680908367@gmail.com>
2023-09-19 22:31:01 +0300

    Created contact section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import Services from "./components/services/Services";
 import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <About />
             <Works />
             <Services />
+            <Contact />
             <Footer />
         </div>
     );
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from "./Contact.module.css";
+
+function Contact() {
+    const [data, setData] = useState({ email: "", text: "" });
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        alert(JSON.stringify(data));
+        setData({ email: "", text: "" });
+    }
+
+    function handleInputChange(event, name) {
+        setData({ ...data, [name]: event.target.value });
+    }
+
+    return (
+        <div className={styles.contact}>
+            <h1>Contact With Me</h1>
+            <form onSubmit={handleFormSubmit} className={styles.contact__form}>
+                <div className={styles.form__block}>
+                    <label>
+                        E-mail:
+                        <input
+                            type="text"
+                            value={data.email}
+                            onChange={(event) =>
+                                handleInputChange(event, "email")
+                            }
+                            className={styles.form__input}
+                        />
+                    </label>
+                </div>
+                <div className={styles.form__block}>
+                    <label>
+                        Your Message:
+                        <textarea
+                            type="text"
+                            value={data.text}
+                            onChange={(event) =>
+                                handleInputChange(event, "text")
+                            }
+                            className={styles.form__input}
+                        ></textarea>
+                    </label>
+                </div>
+                <button type="submit" className={styles.form__button}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Contact;
